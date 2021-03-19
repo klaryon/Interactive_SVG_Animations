18cf958eb458745e141741dbff2e43fbd0538b41
klaryon <klary.ardon@gmail.com>
2021-03-19 11:05:00 +0100

    path wobble added as a mask

--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-// moves from existing value to some new value
-// gsap.to('.rectangle', {
-//     xPercent: 100
-// })
-
-// moves from some values to its existing value
-// gsap.from('.rectangles', {
-//     yPercent: 100,
-//     ease: "expo.out",
-//     duration: 2,
-//     stagger: {
-//         amount: 1,
-//         from: "random"
-//     },
-//     scrollTrigger: "#candy"
-// });
-
 gsap.from('#wobble', {
     xPercent: 100,
     yPercent: 100,
