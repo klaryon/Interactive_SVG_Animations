067503c5a166fbc0ed15d3550b506f31face2d81
klaryon <klary.ardon@gmail.com>
2021-03-19 11:27:35 +0100

    animationIsOkay enable-disable

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
-gsap.from('#wobble', {
+const animationIsOkay = window.matchMedia(
+  "(prefers-reduced-motion: no-preference)"
+).matches;
+
+//turn this on to prevent animation for people who don't want it
+// if (animationIsOkay) {
+  gsap.from("#wobble", {
     xPercent: 100,
     yPercent: 100,
     duration: 2,
-    scrollTrigger: "#candy"
-});
+    scrollTrigger: {
+      trigger: "#candy",
+      pin: true,
+      scrub: 1
+    },
+  });
+// }
