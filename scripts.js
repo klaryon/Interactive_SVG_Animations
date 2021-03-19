642389aef9a9ebc5d655cebacdde04c1d778a638
klaryon <klary.ardon@gmail.com>
2021-03-19 09:17:20 +0100

    greensock and image  added

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// moves from existing value to some new value
+// gsap.to('.rectangle', {
+//     xPercent: 100
+// })
+
+// moves from some values to its existing value
+gsap.from('.rectangle', {
+    xPercent: 100,
+    ease: "expo.out",
+    duration: 2
+})
